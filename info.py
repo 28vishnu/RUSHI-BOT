080abf99e40ed545ba3c0526ca4c81c9994c5328
28vishnu <129520592+28vishnu@users.noreply.github.com>
2025-08-04 21:57:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'AgACAgUAAxkBAAICuGht-6GMOH1PNyf31mjemh8PCzcYAALPxDEbEDJwV5LouvHkzjTeAAgBAAMCAAN4AAceB')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/E8K.jpg')).split()
 
 
 # Admins & Users
