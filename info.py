448f7695c9ceb5bcd9fdaed1bc9bd4a9c6baac1b
28vishnu <129520592+28vishnu@users.noreply.github.com>
2025-08-04 22:26:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+OO2aYpEdXodlYTk1')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviegroup24h')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/rushibhai5k')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/cinebotofclgrup') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/luffydev2k')
